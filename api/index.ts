bb98518e9438cfe5740c50aa7a067f39c7c8edae
Leonardo <leonardo.feilstrecker@gx2.com.br>
2026-01-17 14:53:31 -0300

    fix: adicionado /api para vercel

---
+++ api/index.ts
@@ -0,0 +1,11 @@
+import { app } from '../src/shared/infra/http/app';
+
+app.listen(3000, '0.0.0.0', async () => {
+  console.log('Connected to database.');
+  console.log(`Server is running on port ${3000}`);
+
+  const parentType = process.env.PARENT_TYPE;
+  const childUrls = process.env.CHILD_URLS ?? [];
+
+  console.log({ childUrls, parentType });
+});
